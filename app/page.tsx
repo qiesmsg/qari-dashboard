4af29eb4cd40af1470bc9f60d228425dae73f538
qiesmsg <qiesmsg@gmail.com>
2025-04-04 01:30:17 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "100px" }}>
+      <h1>💠 QARI Dashboard is Live!</h1>
+      <p>Welcome! This is the new landing page using App Router.</p>
+      <a href="/QARIDashboard">Go to Dashboard</a>
+    </div>
+  );
+}
