c9334408fb382f4ba610bcb903b1de2b1436ea09
qiesmsg <qiesmsg@gmail.com>
2025-04-04 04:53:31 +0800

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "100px" }}>
-      <h1>🌟 QARI Dashboard is Live!</h1>
-      <p>Welcome to the staking system.</p>
-      <a href="/QARIDashboard">Launch the Dashboard</a>
-    </div>
-  );
-}
